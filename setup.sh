40294ce7c1a52c65b191ff569518df7fb05351ea
Muntasir Billah Munna <kmmuntasir@gmail.com>
2024-08-08 11:02:20 +0600

    Update Display pattern

--- setup.sh
+++ setup.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Sourcing the variables
+
+echo "=========================="
+echo "Sourcing the variables"
+echo "=========================="
 source ./config.sh
 
-# Create the SSH keys
+echo "=========================="
+echo "Create the SSH keys"
+echo "=========================="
 mkdir $HOME/.ssh # Only if it's not there already
 ssh-keygen -t ed25519 -C "$PERSONAL_EMAIL" -f $HOME/.ssh/key_personal
 ssh-keygen -t ed25519 -C "$WORK_EMAIL" -f $HOME/.ssh/key_work
 
-# Start the SSH agent and add the keys
+echo "=========================="
+echo "Start the SSH agent and add the keys"
+echo "=========================="
 eval "$(ssh-agent -s)" # Raise the SSH flag!
 ssh-add ~/.ssh/key_personal # Toss in your personal key
 ssh-add ~/.ssh/key_work # And now the work key
 
-# Create the SSH config files
+echo "=========================="
+echo "Create the SSH config files"
+echo "=========================="
 echo "Host github.com
           HostName github.com
           Port 22
@@ -25,16 +34,22 @@ echo "Host github.com
           User git
           IdentityFile ~/.ssh/key_work" > $HOME/.ssh/config_work
 
-# Set the permissions
+echo "=========================="
+echo "Set the permissions"
+echo "=========================="
 sudo chmod 600 ~/.ssh/config_personal
 sudo chmod 600 ~/.ssh/config_work
 
-# Set the Git configurations
+echo "=========================="
+echo "Set the Git configurations"
+echo "=========================="
 source ./set_git_config.sh
 
-# Check the configurations
+echo "=========================="
+echo "Check the Git configurations"
+echo "=========================="
 echo 'Personal Git Configuration:'
-cd $HOME && git config --show-origin --get user.email
+cd $PERSONAL_DIRECTORY && git config --show-origin --get user.email
 
-#echo 'Work Git Configuration:'
+echo 'Work Git Configuration:'
 cd $WORK_DIRECTORY && git config --show-origin --get user.email
